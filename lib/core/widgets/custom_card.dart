1976b35b924466caa85500b2f761d97ecd3a17b1
ylruiz <yunet.lruiz94@gmail.com>
2026-01-27 18:43:47 +0100

    clean up CustomCard

--- lib/core/widgets/custom_card.dart
+++ lib/core/widgets/custom_card.dart
@@ -30,9 +30,6 @@ class CustomCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final colorScheme = theme.colorScheme;
-
     return Container(
       width: width,
       height: height,
